483e2eb1f701d02afce1ca2b7580b9b12ec45cff
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-02 23:28:20 +0300

    fix: small fix migrations

--- db/migrate/20240502200447_create_tests.rb
+++ db/migrate/20240502200447_create_tests.rb
@@ -2,8 +2,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title
-      t.string :level
-      t.references :category_id, null: false, foreign_key: true
+      t.integer :level
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20240502200612_create_answers.rb
+++ db/migrate/20240502200612_create_answers.rb
@@ -2,7 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.text :body
-      t.references :question_id, null: false, foreign_key: true
+      t.boolean :correct
+      t.references :question, null: false, foreign_key: true
 
       t.timestamps
     end
