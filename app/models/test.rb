6ad74e73a2b8ac52ff34ffd11eba2852425255e8
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-15 14:37:39 +0300

    feat: add associations to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+
   def self.tests_by_category(category_title)
-    joins('JOIN categories ON categories.id = tests.category_id')
+    joins(:category)
       .where(categories: {title: category_title})
       .order(id: :desc)
       .pluck(:title)
