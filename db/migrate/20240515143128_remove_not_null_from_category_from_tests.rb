9c3c961fc195135653672bead203f599efdb546d
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-16 11:37:34 +0300

    fix: remove last migration

--- db/migrate/20240515143128_remove_not_null_from_category_from_tests.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNotNullFromCategoryFromTests < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :tests, :category_id, true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_15_143128) do
+ActiveRecord::Schema.define(version: 2024_05_14_092627) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2024_05_15_143128) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "author_id", null: false
-    t.integer "category_id"
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
