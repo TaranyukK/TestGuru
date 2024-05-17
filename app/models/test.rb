8dee87e3b952e4f40f234303256f3f411354f559
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-17 14:49:05 +0300

    feat: add scopes to models; refactor models methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,13 @@ class Test < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
+  scope :easy_level, -> { where(level: 0..1) }
+  scope :medium_level, -> { where(level: 2..4) }
+  scope :hard_level, -> { where(level: 5..Float::INFINITY) }
+
+  scope :by_category, -> (category_title) { joins(:category).where(categories: { title: category_title }) }
+
   def self.tests_by_category(category_title)
-    joins(:category)
-      .where(categories: {title: category_title})
-      .order(id: :desc)
-      .pluck(:title)
+    by_category(category_title).order(id: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
   has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :destroy
 
-  def tests_by_level(level)
-    tests.where(level: level)
-  end
+  scope :tests_by_level, -> (level) { where(level: level) }
 end
