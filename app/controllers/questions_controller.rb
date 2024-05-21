0e08a77bc1624adb1df94ed4b654379d0dd3c54b
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-21 22:34:29 +0300

    feat: add shallow: true to questions in routes.rb; small edit in controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_test
+  before_action :set_test, only: [:index, :new, :create]
   before_action :set_question, only: [:show, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -9,6 +9,8 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
+
     render plain: @question.body
   end
 
@@ -34,7 +36,7 @@ class QuestionsController < ApplicationController
   end
 
   def set_question
-    @question = @test.questions.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def question_params
