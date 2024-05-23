12878c839c8c24f577b720bbf8a8d247b56fb4af
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-24 00:08:27 +0300

    fix: fix sending method: :delete in link_to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  private
-
-  def rescue_with_question_not_found
-    render html: "<h1>Not Found</h1>".html_safe, status: :not_found
-  end
 end
