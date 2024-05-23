aa425e006a55a04f9b57ed58ad76b8a0990bb1fb
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-23 13:43:59 +0300

    feat: edit questions controller; add view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,36 +1,45 @@
 class QuestionsController < ApplicationController
   before_action :set_test, only: [:index, :new, :create]
-  before_action :set_question, only: [:show, :destroy]
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  # rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render plain: @test.questions.map(&:body)
+    @questions = @test.questions
   end
 
   def show
-    @question = Question.find(params[:id])
-
-    render plain: @question.body
   end
 
   def new
+    @question = @test.questions.new
+  end
+
+  def edit
   end
 
   def create
     @question = @test.questions.build(question_params)
 
     if @question.save
-      render plain: "Question: '#{@question.body}' created!"
+      redirect_to @question
     else
-      render plain: @question.errors.full_messages
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
   def destroy
     @question.destroy
 
-    render plain: "Question deleted!"
+    redirect_to test_questions_path(@question.test)
   end
 
   private
