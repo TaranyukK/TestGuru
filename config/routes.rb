0aa95641792fb6c33de2281a3a838b01a278d34d
TaranyukK <taranyuk.kirill@gmail.com>
2024-05-30 02:12:37 +0300

    feat: add pages_controller.rb and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'tests#index'
+  root 'pages#index'
 
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: :create
   resources :sessions, only: :create
 
-  resources :tests, except: :index do
+  resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
