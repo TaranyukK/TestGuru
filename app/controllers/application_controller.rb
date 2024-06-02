4ddb47adf36140eb3e477f6f3ba09da16ab73c98
TaranyukK <taranyuk.kirill@gmail.com>
2024-06-03 02:26:05 +0300

    feat: add url options; set default locale to ru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 
   protected
 
@@ -14,5 +19,11 @@ class ApplicationController < ActionController::Base
     else
       stored_location_for(resource) || root_path
     end
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
