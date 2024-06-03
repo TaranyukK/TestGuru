a2c7ea6326361155212505b6e410f0642e318e99
TaranyukK <taranyuk.kirill@gmail.com>
2024-06-03 17:36:30 +0300

    feat: add styles for errors and flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,14 @@ module ApplicationHelper
 
   def flash_messages
     flash.map do |type, message|
-      content_tag(:p, message, class: "flash #{type}")
+      bootstrap_class = case type.to_sym
+                        when :notice then 'alert alert-success'
+                        when :alert then 'alert alert-danger'
+                        when :error then 'alert alert-danger'
+                        when :warning then 'alert alert-warning'
+                        else 'alert alert-info'
+                        end
+      content_tag(:div, message, class: bootstrap_class)
     end.join.html_safe
   end
 end
