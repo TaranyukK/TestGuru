2b711a5448a815defdd269da233e4e57d8f8876a
TaranyukK <taranyuk.kirill@gmail.com>
2024-06-21 02:06:46 +0300

    fix: fix render script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails webpacker:install
 ./bin/webpack-dev-server
 bundle exec rails db:migrate
 bundle exec rails db:seed
